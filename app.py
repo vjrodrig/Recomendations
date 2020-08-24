19dc24318df5471a3f7a4de6adf8dc19a04d0f3f
vjrodrig <53572695+vjrodrig@users.noreply.github.com>
2020-08-24 09:31:53 -0400

    arregla alpha total y agrega cache

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ st.markdown(
 
 df1=pd.read_csv('recom.csv')
 df1['Date']=pd.to_datetime(df1['Date'])
+df1['Account_Id']=pd.to_numeric(df1['Account_Id'])
 igpa_inx=pd.read_csv('igpa.csv')
 
 recom = pd.pivot_table(data=df1, index=['Issuer_Local_Industry','An_Name','Issuer_Compass_Alias','Type','Account_Id'],columns=['Date'], values=['Value'])
@@ -64,9 +65,9 @@ option=st.sidebar.selectbox(
     'Select an option:',
     indus)
 'You Selected:',option
+i=option
 
-
-#@st.cache(suppress_st_warning=True)
+@st.cache(suppress_st_warning=True)
 def charter(i):
 
     recom2=recom[recom.columns[recom.columns.levels[1]>datetime.strptime('01/01/20', '%m/%d/%y')]] # selecciona mayores a una fecha
@@ -75,74 +76,152 @@ def charter(i):
     igpa_inx['Date']=pd.to_datetime(igpa_inx['Date'])
     igpa_inx2=igpa_inx.set_index('Date')
     igpa_inx2=igpa_inx2.reindex(xvals)
-    igpa_v=np.array(igpa_inx2.value)/np.array(igpa_inx2.value)[0]
+    # igpa_v=np.array(igpa_inx2.value)/np.array(igpa_inx2.value)[0]
 
     companies=list(ind.index.get_level_values(0).unique())
     long=len(companies)//2+1
 
-    plt.figure(figsize=(10,4*long))
-    for c, value in enumerate(companies,1):
+    return ind,companies,long,igpa_inx2,xvals
+
+params=charter(option)
+
+plt.figure(figsize=(10,4*params[2]))
+for c, value in enumerate(params[1],1):
     
-        company=ind.loc[value]
-        price=company.loc(axis=0)[:,[39]]
-        price=price.T.fillna(method='ffill').loc['Value']
+    company=params[0].loc[value]
+    price=company.loc(axis=0)[:,[39]]
+    price=price.T.fillna(method='ffill').loc['Value']
 
-        company=ind.loc[value]
-        rec=company.loc(axis=0)[:,[43]]
-        rec=rec.T.fillna(method='ffill').loc['Value']
+    company=params[0].loc[value]
+    rec=company.loc(axis=0)[:,[43]]
+    rec=rec.T.fillna(method='ffill').loc['Value']
         
-        a=np.array(price.loc[:,'Actual'])
-        e=np.array(price.loc[:,'Estimates'])
+    a=np.array(price.loc[:,'Actual'])
+    e=np.array(price.loc[:,'Estimates'])
         
-        rec1=rec.replace(0,np.nan)
-        rec1=rec1.replace(-1,np.nan)
-        buy=np.array(rec1)
-        buy=buy*e
+    rec1=rec.replace(0,np.nan)
+    rec1=rec1.replace(-1,np.nan)
+    buy=np.array(rec1)
+    buy=buy*e
 
-        rec2=rec.replace(1,np.nan)
-        rec2=rec2.replace(-1,np.nan)
-        rec2=rec2.replace(0,1)
-        hold=np.array(rec2)
-        hold=hold*e
+    rec2=rec.replace(1,np.nan)
+    rec2=rec2.replace(-1,np.nan)
+    rec2=rec2.replace(0,1)
+    hold=np.array(rec2)
+    hold=hold*e
 
-        rec3=rec.replace(1,np.nan)
-        rec3=rec3.replace(0,np.nan)
-        rec3=rec3.replace(-1,1)
-        sell=np.array(rec3)
-        sell=sell*e
+    rec3=rec.replace(1,np.nan)
+    rec3=rec3.replace(0,np.nan)
+    rec3=rec3.replace(-1,1)
+    sell=np.array(rec3)
+    sell=sell*e
         
-        igpa_v2=np.array(igpa_inx2.value)/np.array(igpa_inx2.value)[0]*a[0]
+    igpa_v2=np.array(params[3].value)/np.array(params[3].value)[0]*a[0]
         
-        datos=pd.DataFrame()
-        datos['a']=price.Actual.loc[:,39]
-        datos['rec']=rec
-        datos['idx']=igpa_v2
-        datos['a_var']=datos['a'].pct_change()*datos['rec']
-        datos['idx_var']=datos['idx'].pct_change()*datos['rec']
+    datos=pd.DataFrame()
+    datos['a']=price.Actual.loc[:,39]
+    datos['rec']=rec
+    datos['idx']=igpa_v2
+    datos['a_var']=datos['a'].pct_change()*datos['rec']
+    datos['idx_var']=datos['idx'].pct_change()*datos['rec']
+    # datos['alpha']=datos['a_var']-datos['idx_var']
         #datos=datos.dropna()
     
-        upside=round((price.loc[price.index.max()][1]/price.loc[price.index.max()][0]-1)*100,1)
-        ret_idx=datos['idx_var'].add(1).cumprod().iloc[-1]-1
-        ret_acc=datos['a_var'].add(1).cumprod().iloc[-1]-1
-        alpha_acum=round((ret_acc-ret_idx)*100,1)
+    upside=round((price.loc[price.index.max()][1]/price.loc[price.index.max()][0]-1)*100,1)
+    ret_idx=datos['idx_var'].sum()#.add(1).cumprod().iloc[-1]-1
+    ret_acc=datos['a_var'].sum()#.add(1).cumprod().iloc[-1]-1
+    # alpha_acum=(datos['alpha'].add(1).cumprod().iloc[-1]-1)*100
+    alpha_acum=round((ret_acc-ret_idx)*100,1)
 
-        plt.subplot(long,2,c)
-        plt.title(str(value)+' \n (alpha ytd: '+str(alpha_acum)+'%)'+'\n (upside:'+str(upside)+'%)', size=10)
+    plt.subplot(params[2],2,c)
+    plt.title(str(value)+' \n (alpha ytd: '+str(alpha_acum)+'%)'+'\n (upside:'+str(upside)+'%)', size=10)
 
-        plt.plot(xvals,igpa_v2, color='blue',alpha=1,linewidth=1,linestyle='--',label='igpa')
-        plt.plot(xvals,a, color='black',alpha=0.8)
-        plt.plot(xvals,e, color='grey',alpha=0.8, linestyle='--')
-        plt.plot(xvals,buy, color='green',alpha=1,linewidth=3)
-        plt.plot(xvals,hold, color='grey',alpha=1,linewidth=3)
-        plt.plot(xvals,sell, color='red',alpha=1,linewidth=3)
+    plt.plot(params[4],igpa_v2, color='blue',alpha=1,linewidth=1,linestyle='--',label='igpa')
+    plt.plot(params[4],a, color='black',alpha=0.8)
+    plt.plot(params[4],e, color='grey',alpha=0.8, linestyle='--')
+    plt.plot(params[4],buy, color='green',alpha=1,linewidth=3)
+    plt.plot(params[4],hold, color='grey',alpha=1,linewidth=3)
+    plt.plot(params[4],sell, color='red',alpha=1,linewidth=3)
 
-        plt.tick_params(labelrotation=90,axis='x')
-        plt.tick_params(labelsize=8)
+    plt.tick_params(labelrotation=90,axis='x')
+    plt.tick_params(labelsize=8)
 
-        plt.legend()
-        plt.tight_layout()
+    plt.legend()
+    plt.tight_layout()
+st.pyplot()
+
+
+
+# recom2=recom[recom.columns[recom.columns.levels[1]>datetime.strptime('01/01/20', '%m/%d/%y')]] # selecciona mayores a una fecha
+# ind=recom2.loc[i]
+# xvals=list(ind.columns.get_level_values(1))
+# igpa_inx['Date']=pd.to_datetime(igpa_inx['Date'])
+# igpa_inx2=igpa_inx.set_index('Date')
+# igpa_inx2=igpa_inx2.reindex(xvals)
+#     # igpa_v=np.array(igpa_inx2.value)/np.array(igpa_inx2.value)[0]
+
+# companies=list(ind.index.get_level_values(0).unique())
+# long=len(companies)//2+1
+
+# plt.figure(figsize=(10,4*long))
+# for c, value in enumerate(companies,2):
+    
+#     company=ind.loc[value]
+#     price=company.loc(axis=0)[:,[39]]
+#     price=price.T.fillna(method='ffill').loc['Value']
+
+#     company=ind.loc[value]
+#     rec=company.loc(axis=0)[:,[43]]
+#     rec=rec.T.fillna(method='ffill').loc['Value']
         
+#     a=np.array(price.loc[:,'Actual'])
+#     e=np.array(price.loc[:,'Estimates'])
+        
+#     rec1=rec.replace(0,np.nan)
+#     rec1=rec1.replace(-1,np.nan)
+#     buy=np.array(rec1)
+#     buy=buy*e
 
-    return st.pyplot()
+#     rec2=rec.replace(1,np.nan)
+#     rec2=rec2.replace(-1,np.nan)
+#     rec2=rec2.replace(0,1)
+#     hold=np.array(rec2)
+#     hold=hold*e
 
-charter(option)
+#     rec3=rec.replace(1,np.nan)
+#     rec3=rec3.replace(0,np.nan)
+#     rec3=rec3.replace(-1,1)
+#     sell=np.array(rec3)
+#     sell=sell*e
+        
+#     igpa_v2=np.array(igpa_inx2.value)/np.array(igpa_inx2.value)[0]*a[0]
+        
+#     datos=pd.DataFrame()
+#     datos['a']=price.Actual.loc[:,39]
+#     datos['rec']=rec
+#     datos['idx']=igpa_v2
+#     datos['a_var']=datos['a'].pct_change()*datos['rec']
+#     datos['idx_var']=datos['idx'].pct_change()*datos['rec']
+#         #datos=datos.dropna()
+    
+#     upside=round((price.loc[price.index.max()][1]/price.loc[price.index.max()][0]-1)*100,1)
+#     ret_idx=datos['idx_var'].add(1).cumprod().iloc[-1]-1
+#     ret_acc=datos['a_var'].add(1).cumprod().iloc[-1]-1
+#     alpha_acum=round((ret_acc-ret_idx)*100,1)
+
+#     plt.subplot(long,2,c)
+#     plt.title(str(value)+' \n (alpha ytd: '+str(alpha_acum)+'%)'+'\n (upside:'+str(upside)+'%)', size=10)
+
+#     plt.plot(xvals,igpa_v2, color='blue',alpha=1,linewidth=1,linestyle='--',label='igpa')
+#     plt.plot(xvals,a, color='black',alpha=0.8)
+#     plt.plot(xvals,e, color='grey',alpha=0.8, linestyle='--')
+#     plt.plot(xvals,buy, color='green',alpha=1,linewidth=3)
+#     plt.plot(xvals,hold, color='grey',alpha=1,linewidth=3)
+#     plt.plot(xvals,sell, color='red',alpha=1,linewidth=3)
+
+#     plt.tick_params(labelrotation=90,axis='x')
+#     plt.tick_params(labelsize=8)
+
+#     plt.legend()
+#     plt.tight_layout()
+# st.pyplot()
